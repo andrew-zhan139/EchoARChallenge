bc2627f4d18892215138895136f9f944f67ecb3d
andrewzhan <andrewzhan8@gmail.com>
2020-10-18 00:48:03 -0400

    hehhehehhehehehe

---
+++ Assets/agorainterface.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using agora_gaming_rtc;
+
+public class agorainterface : MonoBehaviour
+{
+    private static string appid = "fce15e90dba8416cad27d3cf6d20055f";
+    public IRtcEngine mRtcEngine;
+    public uint mRemotePeer;
+    public void loadEngine () {
+        Debug.Log("starting engine");
+        if (mRtcEngine != null) {
+            Debug.Log("engine exists");
+            return;
+
+        }
+        mRtcEngine = IRtcEngine.getEngine(appid);
+        mRtcEngine.SetLogFilter(LOG_FILTER.DEBUG);
+
+    }
+    public void joinChannel(string channelname) {
+        Debug.Log("joining channel " + channelname);
+
+        if (mRtcEngine== null) {
+            Debug.Log("engine needs to be initialized");
+            return;
+
+        }
+        mRtcEngine.OnJoinChannelSuccess = OnJoinChannelSuccess;
+        mRtcEngine.OnUserJoined = OnUserJoined;
+        mRtcEngine.OnUserOffline = OnUserOffline;
+
+
+        mRtcEngine.EnableVideo();
+        mRtcEngine.EnableVideoObserver();
+        mRtcEngine.JoinChannel(channelname, null, 0);
+
+    }
+
+    public void leaveChannel() {
+        Debug.Log("leaving channel");
+        if (mRtcEngine == null) {
+            Debug.Log("missing engine");
+            return;
+        }
+        mRtcEngine.LeaveChannel();
+        mRtcEngine.DisableVideoObserver();
+
+    }
+    public void unloadEngine() {
+        Debug.Log("unloading engine");
+        if (mRtcEngine != null ) {
+            IRtcEngine.Destroy();
+            mRtcEngine = null;
+        }
+    }
+
+   /* public void getsdkversion() {
+        Debug.Log("getting version");
+        GameObject versiontext = GameObject.Find("")
+    }*/
+
+    private void OnJoinChannelSuccess (string channelname, uint uid, int elapsed) {
+        Debug.Log("success join channel " + channelname + " " + uid);
+
+    } 
+    private void OnUserJoined (uint uid, int elapsed) {
+        Debug.Log("new person join channel " + " " + uid);
+        GameObject go;
+        go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        go.name = uid.ToString();
+        VideoSurface o = go.AddComponent<VideoSurface>();
+        o.SetForUser(uid);
+        o.mAdjustTransfrom += OnTransformDelegate;
+        o.SetEnable(true);
+        o.transform.Rotate(-90.0f, 0.0f, 0.0f);
+        float r = Random.Range(-5.0f, 5.0f);
+        o.transform.position = new Vector3(0f, r, 0f);
+        o.transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
+        mRemotePeer = uid;
+
+    }
+
+    private void OnUserOffline (uint uid, USER_OFFLINE_REASON reason) {
+        Debug.Log("user off line with id " + uid);
+        GameObject go = GameObject.Find(uid.ToString());
+        if (!ReferenceEquals(go, null)) {
+            Destroy(go);
+        }
+
+
+    }
+
+    private void OnTransformDelegate (uint uid, string objname, ref Transform transform) {
+        if (uid == 0) {
+            transform.position = new Vector3(0f, 2f, 0f);
+            transform.localScale = new Vector3(2.0f, 2.0f, 1.0f);
+            transform.Rotate(0f, 1f, 0f);
+
+        } else {
+            transform.Rotate(0.0f, 1.0f, 0.0f);
+        }
+    }
+    public void OnChatSceneLoaded() {
+        GameObject go = GameObject.Find("Sphere");
+        if (ReferenceEquals(go, null)) {
+            Debug.Log("cant fiund sphere");
+            return;
+        }
+
+        VideoSurface o =  go.GetComponent<VideoSurface>();
+        o.mAdjustTransfrom += OnTransformDelegate;
+    }
+}
---
+++ Assets/buttonhandle.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+#if(UNITY_2018_3_OR_NEWER)
+using UnityEngine.Android;
+#endif
+ 
+public class buttonhandle : MonoBehaviour
+{
+    static agorainterface app = null; 
+    // Start is called before the first frame update
+    void Start()
+    {
+#if (UNITY_2018_3_OR_NEWER)     
+        if (Permission.HasUserAuthorizedPermission(Permission.Microphone)) {
+
+        } else {
+            Permission.RequestUserPermission(Permission.Microphone);
+        }
+
+        if (Permission.HasUserAuthorizedPermission(Permission.Camera)) {
+
+        } else {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClick (){
+        Debug.Log("Button Clicked: " + name);
+        if (name.CompareTo("joinbutton") == 0) {
+
+        } else if (name.CompareTo("leavebutton") == 0) {
+
+        }
+    }
+
+    private void OnJoinButtonClicked () {
+        Debug.Log("join clicked");
+
+        GameObject go = GameObject.Find("channelname");
+        InputField input = go.GetComponent<InputField>();
+
+        if (ReferenceEquals(app, null)) {
+            app = new agorainterface();
+            app.loadEngine();
+        }
+
+        app.joinChannel(input.text);
+        SceneManager.sceneLoaded += OnSceneFinishedLoading;
+        SceneManager.LoadScene("callscreen", LoadSceneMode.Single);
+        
+    }
+    private void OnLeaveButtonClicked () {
+        Debug.Log("leave clicked");
+        if (ReferenceEquals(app, null)) {
+            app.leaveChannel();
+            app.unloadEngine();
+            app = null;
+            SceneManager.LoadScene("JoinScreen", LoadSceneMode.Single);
+
+        }
+    }
+
+    public void OnSceneFinishedLoading (Scene scene, LoadSceneMode mode) {
+        if (scene.name.CompareTo("callscreen") == 0) {
+            if (!ReferenceEquals(app, null)) {
+                app.OnChatSceneLoaded();
+            }
+            SceneManager.sceneLoaded -= OnSceneFinishedLoading;
+        }
+    }
+    
+}
